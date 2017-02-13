b81e6f47a28d343a8ca99561ca9b227ce53ab841
Taras Sokol <tsokolp@gmail.com>
2017-02-14 00:35:55 +0300

    add ticket-user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213205603) do
+ActiveRecord::Schema.define(version: 20170213212839) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -29,12 +29,25 @@ ActiveRecord::Schema.define(version: 20170213205603) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "route_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
