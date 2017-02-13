3698f787680432334aef03c4ff78fd293b87ef24
Taras Sokol <tsokolp@gmail.com>
2017-02-14 00:02:47 +0300

    add train-route association

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ActiveRecord::Base
+  has_many :trains
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
   validates :name, presence: true
