4c671123ff21b57fee4e86998ef3496a4d6a77be
Taras Sokol <tsokolp@gmail.com>
2017-02-17 01:25:33 +0300

    add optional: true in train model

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,4 @@
 class Train < ApplicationRecord
-  belongs_to :route
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :route, optional: true
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
 end
