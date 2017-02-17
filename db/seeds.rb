b0151de8ed9f1025490823bd9d6dbd89add46572
Taras Sokol <tsokolp@gmail.com>
2017-02-17 21:23:44 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+RailwayStation.create(title: 'Moscow')
+RailwayStation.create(title: 'Obninsk')
+RailwayStation.create(title: 'Balabanovo')
+RailwayStation.create(title: 'Naro-Fominsk')
+RailwayStation.create(title: 'Skolkovo')
+RailwayStation.create(title: 'Aprelevka')
+
+Train.create(number: 'z5649Gh')
+Train.create(number: 'ty7778Q')
+Train.create(number: 'ML8097y')
+Train.create(number: 'vX6789H')
+Train.create(number: 'Sd564gJ')
+Train.create(number: 'as34fg6')
+
+Route.create(name: 'Obninsk - Moscow')
+Route.create(name: 'Naro-Fominsk - Moscow')
+
+
+
