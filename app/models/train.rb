b9de6ffff5e3c21b7e03da08c88d285cfb1a495a
Taras Sokol <tsokolp@gmail.com>
2017-02-18 13:30:49 +0300

    ticket model bugfix

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,4 +2,5 @@ class Train < ApplicationRecord
   belongs_to :route, optional: true
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
   has_many :carriages
+  has_many :tickets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214185855) do
+ActiveRecord::Schema.define(version: 20170218102540) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 20170214185855) do
     t.string   "number"
     t.integer  "start_station_id"
     t.integer  "end_station_id"
+    t.integer  "train_id"
     t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
     t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
+    t.index ["train_id"], name: "index_tickets_on_train_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
