09bdfc4ab332e78d2788f6df272601ad7481fbb9
falcont <tsokolp@gmail.com>
2017-03-20 21:41:24 +0000

    add carriages details to train view

--- app/helpers/trains_helper.rb
+++ app/helpers/trains_helper.rb
@@ -1,2 +1,13 @@
 module TrainsHelper
+  def show_carriages(train)
+    content_tag(:p, class: "carriages") do
+      train.carriages.each do |carriage|
+        concat(link_to("#{carriage.number}", sti_carriage_path(carriage.type, carriage)))
+        concat tag(:br)
+    end
+   end
+  end
 end
+
+
+
