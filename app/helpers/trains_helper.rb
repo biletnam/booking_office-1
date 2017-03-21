fd08a19f1aedeb2db1c58d8aac930071215df4e5
falcont <tsokolp@gmail.com>
2017-03-21 20:41:55 +0000

    add calculate seats method to Train model

--- app/helpers/trains_helper.rb
+++ app/helpers/trains_helper.rb
@@ -6,7 +6,7 @@ module TrainsHelper
       @sorted_carriages.each do |carriage|
         concat(link_to("#{carriage.number}", sti_carriage_path(carriage.type, carriage)))
         concat tag(:br)
-    end
+      end
    end
   end
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -3,5 +3,9 @@ class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
   has_many :carriages
   has_many :tickets
+  
+  def calculate_seats(carriage_type, seats_type)
+    self.carriages.where(type: carriage_type).sum(seats_type)
+  end
 
 end
