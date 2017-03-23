169041e6f326ebc329c7a50b85f1aa08d1e11b27
falcont <tsokolp@gmail.com>
2017-03-23 23:25:56 +0000

    move js to assets

---
+++ app/assets/javascripts/carriages.js
@@ -0,0 +1,15 @@
+function renderPartial() {
+    var elements = ['CoupeCarriage', 'EconomyCarriage', 'SoftSeatsCarriage', 'LuxeCarriage']
+    var x = document.getElementById("carriage_type").value;
+    
+    document.getElementById(x).style.visibility = 'visible';
+    document.getElementById(x).style.display = 'unset';
+    
+    elements = elements.filter(val => val !== x);
+    
+    elements.forEach(function(entry) {
+         document.getElementById(entry).style.visibility = 'hidden';
+         document.getElementById(entry).style.display = 'none';
+     });
+   }
+    
