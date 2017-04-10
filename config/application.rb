347e6ecd572310992218de31775a4c49477ef689
falcont <tsokolp@gmail.com>
2017-04-10 14:15:26 +0000

    create partials for layouts

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module BookingOffice
     # -- all .rb files in that directory are automatically loaded.
     config.active_record.time_zone_aware_types = [:datetime, :time]
     config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
   end
 end
