c88cda2a0bfeae16ee16cc2919b2cf99d9d50443
falcont <tsokolp@gmail.com>
2017-04-11 10:10:16 +0000

    ticket model bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
   has_many :tickets
-  validates_presence_of :name, :passport, :passenger
+  validates_presence_of :name
 end
