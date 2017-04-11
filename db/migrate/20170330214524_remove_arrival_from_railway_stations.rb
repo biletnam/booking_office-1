675ccdffe8d5c18c8fc7b885d0926b0ba1edf0cc
falcont <tsokolp@gmail.com>
2017-04-11 00:10:13 +0000

    db works

--- db/migrate/20170330214524_remove_arrival_from_railway_stations.rb
+++
@@ -1,6 +0,0 @@
-class RemoveArrivalFromRailwayStations < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :railway_stations, :arrival, :date
-    remove_column :railway_stations, :departure, :date
-  end
-end
